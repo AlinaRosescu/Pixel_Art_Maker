66c930810fdce56e2e88124055d4056fe7a7253b
Rosescu Alina <alina.rosescu@yahoo.com>
2018-03-02 17:12:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//function that creates a new grid from the height and width values introduced by the user
 function makeGrid() {	
 	var height = $("#height").val();
 	var width = $("#width").val();
@@ -16,6 +16,7 @@ function makeGrid() {
 	hideResetBtn();	
 }
 
+//deletes the grid, values inputed by user and hides the reset button
 function deleteGrid() {
 	$("#height").val("");
 	$("#width").val("");
@@ -23,6 +24,7 @@ function deleteGrid() {
 	hideResetBtn();	
 }
 
+//function that takes the value of color input and is used by the user to draw the grid cells
 function draw() {
 	$("table").on("mousedown", "td", function() {
 		var color = $("#color").val();	
@@ -30,15 +32,17 @@ function draw() {
 	});
 }
 
+//sets the cells background color back to white
 function resetGrid() {
 	$("td").css("background-color", "#fff");
 }
 
+//hides or shows the hreset button by toggling
 function hideResetBtn() {
 	$("#reset").toggleClass("resetBtn");
 }
 
-
+//on document ready and on user click we call different functions
 $(function() {
 	$("#submit").click(makeGrid);
 	$("#delete").click(deleteGrid);	
