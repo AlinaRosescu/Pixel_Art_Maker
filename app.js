2deab5d10fb3aa2f4322998e9a9c532d9f44b5f2
Rosescu Alina <alina.rosescu@yahoo.com>
2018-08-30 13:21:22 +0300

    added preview and save image feature

--- app.js
+++ app.js
@@ -1,33 +1,68 @@
+var getCanvas;
+var valuesSet = false;
+
 //function that creates a new grid from the height and width values introduced by the user
-function makeGrid() {	
+function makeGrid() {
+	var table = $("table");
 	var height = $("#height").val();
 	var width = $("#width").val();
-	var table = $("table");	
-	
-	for (var row = 0; row < height; row++) {
-		var tr = $("<tr>");
-		
-		for (var col = 0; col < width; col++) {
-			$("<td></td>").appendTo(tr);
-		}
 
-		tr.appendTo(table);
+	checkValuesAreInserted(height, width);
+
+
+	if (valuesSet === true) {
+		if ($("tbody").children("tr").length > 0) {
+			$("table").empty();
+		}
+		for (var row = 0; row < height; row++) {
+			var tr = $("<tr>");
+
+			for (var col = 0; col < width; col++) {
+				$("<td></td>").appendTo(tr);
+			}
+			tr.appendTo(table);
+		}
+		showBtn();
+	} else {
+		$("#reset-btn, #preview-btn, #save-btn").removeClass("show").addClass("hide");
+		$("table").empty();
 	}
-	hideResetBtn();	
+
 }
 
-//deletes the grid, values inputed by user and hides the reset button
+
+function checkValuesAreInserted(heightVal, widthVal) {
+	$(".left p, .right p").removeClass("show").addClass("hide");
+	if (heightVal == 0 && widthVal == 0) {
+		console.log(widthVal);
+		$(".left p, .right p").removeClass("hide").addClass("show");
+		valuesSet = false;
+	} else if (heightVal == 0) {
+		$(".left p").removeClass("hide").addClass("show");
+		valuesSet = false;
+	} else if (widthVal == 0) {
+		$(".right p").removeClass("hide").addClass("show");
+		valuesSet = false;
+	} else {
+		valuesSet = true;
+	}
+	return valuesSet;
+}
+
+//deletes the grid, values inputed by user and hides the reset, preview and save buttons
 function deleteGrid() {
-	$("#height").val("");
-	$("#width").val("");
-	$("tr").remove();
-	hideResetBtn();	
+	$("#height").val("0");
+	$("#width").val("0");
+	$("#color").val("#000000");
+	$("table").empty();
+	$("#reset-btn, #preview-btn, #save-btn").removeClass("show").addClass("hide");
+	$("div p").removeClass("show").addClass("hide");
 }
 
 //function that takes the value of color input and is used by the user to draw the grid cells
 function draw() {
 	$("table").on("mousedown", "td", function() {
-		var color = $("#color").val();	
+		var color = $("#color").val();
 		$(this).css("background-color", color);
 	});
 }
@@ -37,20 +72,54 @@ function resetGrid() {
 	$("td").css("background-color", "#fff");
 }
 
-//hides or shows the hreset button by toggling
-function hideResetBtn() {
-	$("#reset").toggleClass("resetBtn");
+//hides or shows the buttons by toggling their class
+function showBtn() {
+	$("#reset-btn, #preview-btn, #save-btn").removeClass("hide").addClass("show");
 }
 
+function previewImg() {
+	/*$("#canvas").css("display", "inline-block");
+	$("#preview-image").css("display", "inline-block");
+	$("canvas").css("border", "1px solid red");*/
+
+	if ($("#preview").children("canvas").length > 0) {
+		$("#preview").empty();
+	}
+
+	/*var $canvasClone = $("#canvas").clone();
+	$("#preview-image").append($canvasClone);
+	$canvasClone.addClass("clone");*/
+	/*$(".clone td").css("border", "none");
+	$(".clone table").css("border", "none");*/
+
+
+	/*console.log($canvasClone);*/
+	html2canvas(document.querySelector("#canvas")).then(canvas => {
+	    $("#preview").append(canvas);
+	    getCanvas = canvas;
+	});
+}
+
+
 //on document ready and on user click we call different functions
-$(function() {
+$(document).ready(function(){
 	$("#submit").click(makeGrid);
-	$("#delete").click(deleteGrid);	
-	$("#reset").click(resetGrid);
-})
+	$("table").click(draw);
+
+	$("#delete-btn").click(deleteGrid);
+	$("#reset-btn").click(resetGrid);
+
+    $("#preview-btn").click(previewImg);
+
+	$("#save-btn").on('click', function () {
+        var imgageData = getCanvas.toDataURL("image/jpg");
+    	var newData = imgageData.replace(/^data:image\/jpg/, "data:application/octet-stream");
+    	$("#save-btn").attr("download", "pixel_art_image.jpg").attr("href", newData);
+	});
+});
 
 
-	
 
-	
+
+
 
